c38d175a4a46bf62e26f2380c8c160a0fee0c12a
angelux <angel@justia.com>
2025-11-28 18:02:10 -0600

    fix: respect data-lazyload attribute on individual elements

--- src/lazyframe.ts
+++ src/lazyframe.ts
@@ -140,7 +140,7 @@ const Lazyframe = () => {
             }
         });
 
-        if (settings.lazyload) {
+        if (instance.settings.lazyload) {
             build(instance);
         } else {
             api(instance);
@@ -310,7 +310,7 @@ const Lazyframe = () => {
             instance.el.appendChild(titleNode);
         }
 
-        if (!settings.lazyload) {
+        if (!instance.settings.lazyload) {
             instance.el.classList.add('lazyframe--loaded');
             if (instance.settings.onLoad) {
                 instance.settings.onLoad(instance);
