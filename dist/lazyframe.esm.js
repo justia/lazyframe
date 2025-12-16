952cbb5bad2e84f39f6d12cb52f1ae17b30e8f40
Diego Miguel <digznav@gmail.com>
2025-12-15 20:37:41 -0600

    chore: build dist files

--- dist/lazyframe.esm.js
+++ dist/lazyframe.esm.js
@@ -130,7 +130,7 @@ const Lazyframe = () => {
             initialized: false, // Always start as not initialized
             built: false,
             originalSrc: dataSrc,
-            useApi: useApi(vendor, restDataAttrs.title, dataLoadThumbnail),
+            useApi: useApi(vendor, restDataAttrs.title, dataThumbnail, loadThumbnail),
             // Parse booleans with defaults and override programmatic options if `data-*` attributes were defined.
             lazyload: parseBoolean(lazyload, programmaticOptions.lazyload),
             autoplay,
@@ -150,24 +150,27 @@ const Lazyframe = () => {
      * Checks if missing data needs to be fetched from the API.
      *
      * The function returns `true` only if a valid Vendor exists, but
-     * the local data is incomplete (missing either a title or a thumbnail).
+     * the local data is incomplete.
      *
      * Logic Matrix:
-     * - No Vendor                -> false
-     * - Vendor + Title + Thumb   -> false (Data complete)
-     * - Vendor + No Title        -> true
-     * - Vendor + No Thumb        -> true
+     * - No Vendor                                     -> false
+     * - Vendor + Title + Thumb                        -> false (Data complete)
+     * - Vendor + Title + (Thumb?) + LoadThumb=False   -> false (Data complete. Thumb ignored)
+     * - Vendor + Title                                -> true  (Need Thumb)
+     * - Vendor + Thumb + LoadThumb=True               -> true  (Need Title)
+     * - Vendor + Thumb + LoadThumb=False              -> true  (Need Title)
      *
      * @param [vendor] - The target vendor.
      * @param [dataTitle] - The current title (if any).
      * @param [thumbnail] - The current thumbnail (if any).
+     * @param [loadThumbnail] - Whether the thumbnail should be displayed.
      * @returns `true` if the API needs to be called to backfill missing data.
      */
-    function useApi(vendor, dataTitle, thumbnail) {
+    function useApi(vendor, dataTitle, thumbnail, loadThumbnail) {
         // Trim ensures we check for actual content.
         const hasTitle = dataTitle?.trim();
         const hasThumb = thumbnail?.trim();
-        return !!vendor && (!hasTitle || !hasThumb);
+        return !!vendor && (!hasTitle || (!!loadThumbnail && !hasThumb));
     }
     // TODO: Extract to another module.
     function parseBoolean(value, defaultValue = false) {
